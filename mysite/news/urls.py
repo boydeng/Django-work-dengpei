33ea0afc1b79581ba4b73135dd7f62b7c4bc36ca
deng pei <shuowenjietu@163.com>
2021-01-13 22:20:42 +0800

    最后提交

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     
     path('articles/<int:year>/', views.year_archive),
-    path('dengpei_yunpan/',views.Dengpei_yunpan_work)
+    path('student_dengpei/add/',views.Dengpei_yunpan_work)
     #path('articles/<int:year>/<int:month>/', views.month_archive),
     #path('articles/<int:year>/<int:month>/<int:pk>/', views.article_detail),
 ]
